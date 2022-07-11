59c3e36d6d897d7df359908cb195fefa97f53639
Petrus Isaksson <petrusisaksson@gmail.com>
2022-07-11 14:09:40 +0200

    Task 1 och början på Task 2

--- Homework_2.py
+++ Homework_2.py
@@ -7,3 +7,19 @@ from numpy import *
 from matplotlib.pyplot import *
 
 "Task 1"
+
+class Interval:
+    def __init__(self,real_left,real_right):
+        self.real_left=real_left
+        self.real_right=real_right
+        
+    "Task 2"
+    
+    def addIntervals(self,other):
+        L1,R1=self.real_left,self.real_right
+        if isinstance(other, Interval):
+            L2,R2=other.real_left,other.real_right
+            return Interval(L1+R2,L2+R1)
+
+    def __repr__(self):
+        return f"self.real_left,self.real_right"                        
