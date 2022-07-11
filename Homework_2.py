97dda1fc1026f45dd10cc1178ea2de357d246c6c
Petrus Isaksson <petrusisaksson@gmail.com>
2022-07-11 18:47:30 +0200

    La till bl.a. Wrong order, om man försöker skriva in intervaller som går uppifrån och ner.

--- Homework_2.py
+++ Homework_2.py
@@ -12,6 +12,10 @@ class Interval:
     def __init__(self,real_left,real_right):
         if isinstance(real_left,complex):
             raise TypeError("Wrong type!")
+        elif isinstance(real_right,complex):
+            raise TypeError("Wrong type!")
+        elif real_left > real_right:
+            raise TypeError("Wrong order!")
         self.real_left=real_left
         self.real_right=real_right
         
@@ -21,12 +25,22 @@ class Interval:
         L1,R1=self.real_left,self.real_right
         if isinstance(other, Interval):
             L2,R2=other.real_left,other.real_right
-            return Interval(L1+R2,L2+R1)
-
+            return Interval(L1+L2,R1+R2)
+        #else:
+            #Interval(self.real_left,self.real_right)+other #förstår inte varför detta inte funkar
+    
+    def __radd__(self, other):
+        L1,R1=self.real_left,self.real_right
+        if isinstance(other, Interval):
+            L2,R2=other.real_left,other.real_right
+            return Interval(L2+L1,R2+R1)
+        #else:
+            #Interval(self.real_left,self.real_right)+other
+    
     def __repr__(self):
         return f'[{self.real_left},{self.real_right}]'
 
-z=Interval(2.,4.)
-q=Interval(3.,7.)
-r=Interval(3+2j,5)    
-print(r+z)                    
+#example run with two intervals
+z=Interval(3.,6.)
+q=Interval(3.,4.) 
+print(z+q)                    
