d8b1426451f014dd68f1a16f0b6bdb45b68e52a8
Petrus Isaksson <petrusisaksson@gmail.com>
2022-07-13 09:12:25 +0200

    Task 7 fixad, men det uppstår problem med TypeError för nedåtgående intervall.

--- Homework_2.py
+++ Homework_2.py
@@ -9,16 +9,16 @@ from matplotlib.pyplot import *
 "Task 1"  
 
 class Interval:
-    def __init__(self,real_left,real_right):
+    def __init__(self,real_left,real_right=None): #we have set a default value for real_right 
         if isinstance(real_left,complex):
             raise TypeError("Wrong type!")
         elif isinstance(real_right,complex):
             raise TypeError("Wrong type!")
-        elif real_left > real_right:
-            raise TypeError("The interval must ascend!")
+        #elif real_left > real_right:
+        #    raise TypeError("The interval must ascend!")
         self.real_left=real_left
-        self.real_right=real_right
-        
+        self.real_right=real_right if real_right is not None else real_left
+    
     "Task 2"
     #addition
     def __add__(self,other):
@@ -60,6 +60,8 @@ class Interval:
             L2,R2=other.real_left,other.real_right
             if L2==0 or R2==0:
                 raise TypeError("It's not possible to divide by an interval containing zero!")
+            elif min([L1/L2,L1/R2,R1/L2,R1/R2]) - max([L1/L2,L1/R2,R1/L2,R1/R2]) <= -10**100:
+                raise TypeError("The result is an infinitely large interval :)")
             else:
                 return Interval(min([L1/L2,L1/R2,R1/L2,R1/R2]),max([L1/L2,L1/R2,R1/L2,R1/R2]))        
     
@@ -70,13 +72,13 @@ class Interval:
             return True
         else:
             return False
-
+        
 #q=Interval(1.0,4.0)
 #print(5 in q)
                       
 #example run with two intervals
-#q=Interval(4.,3.)
-#z=Interval(1.,4.) 
+#q=Interval(1.,10.)
+#z=Interval(10**-100,4.) 
 #print(q/z)   
         
 "Task 3"
@@ -94,6 +96,12 @@ I2=Interval(-2,-1)
 #print(I1/I2)
 
 "Task 6"
+#contained in Task 2
+
+"Task 7"
+#See Task 2 under __init__.
+
+
 
       
     
