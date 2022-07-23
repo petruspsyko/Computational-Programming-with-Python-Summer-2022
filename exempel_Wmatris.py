e864acf0ec19be5bf0cb784e8008a09c931b946b
Petrus Isaksson <petrusisaksson@gmail.com>
2022-07-23 12:39:48 +0200

    Skapar simplify(A,n) i exempel_Wmatris.py

--- exempel_Wmatris.py
+++ exempel_Wmatris.py
@@ -9,9 +9,24 @@ import imageio as iio
 
 "Exempel för att skapa Wavelet transformation"
 
-glass=iio.imread('glass.jpg',as_gray=True)
+får=iio.imread('får.jpg',as_gray=True)
 
 def Wavelet_trans(A):
+    if isinstance(A,int):
+        raise TypeError("Not a foto!")
+    elif isinstance(A,float):
+        raise TypeError("Not a foto!")
+    elif isinstance(A,complex):
+        raise TypeError("Not a foto!")
+    elif [A.shape][0][0] < 2: # Om A bara har en rad 
+        raise TypeError("Not a valid format!") 
+    elif [A.shape][0][0] == A.size: # Om A endast består av rader - är 1D
+        raise TypeError("Not a valid format!")
+    else:
+        if [A.shape][0][0]%2 != 0: # Om A:s rader är av ojämt antal
+            A=A=A[:[A.shape][0][0]-1,:] # Tar bort en av A:s rader
+        if [A.shape][0][1]%2 != 0: # Om A:s kolonner är av ojämt antal
+            A=A[:,:[A.shape][0][1]-1] # Tar bort en av A:s kolonner
         n=[A.shape][0][0]
         W_n=zeros((n,n))
         r=int(n/2)
@@ -23,11 +38,23 @@ def Wavelet_trans(A):
         for i in range(r):    
             W_n[i][i*2]=W_n[i][2*i+1]=W_n[i+r][2*i+1]=wf
             W_n[i+r][i*2]=-wf
-    
+            
             for k in range(p):
                 W_m[k][k*2]=W_m[k][2*k+1]=W_m[k+p][2*k+1]=wf
                 W_m[k+p][k*2]=-wf
     
-        return matmul((matmul(W_n,A)),W_m.transpose())
+    B=matmul((matmul(W_n,A)),W_m.transpose()) # B består av fyra submatriser
+    
+    B2=zeros((r,p)) # Ny matris för den övre vänstra submatrisen    
+    for i in range(r):
+        for k in range(p):
+                B2[i][k]=B[i][k] # Stoppar det i övre vänstra hörnet i B
+    return B2
 
-iio.imsave('glass.jpg',glass)
+def simplifyfoto(A,n): # Halverar antalet pixlar n gånger
+    for i in range(n):
+        A=Wavelet_trans(A)
+    return A 
+
+#gruppfoto_trans=Wavelet_trans(gruppfoto)
+#iio.imsave('grupfoto_trans.jpg',gruppfoto_trans)
